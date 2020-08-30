55f9bc4e0efbc98d37076a62281e6522898da72f
Spirit_xc <xc1129@gmail.com>
2020-08-30 18:45:44 +0800

    modify for pos

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,6 @@
 var exec = require("child_process").exec;
 
-function start(response) {
+function start(response, postData) {
     console.log("Request handler 'start' was called.");
 
     var body = '<html>'+
@@ -43,10 +43,10 @@ function start(response) {
     */
 }
 
-function upload(response) {
+function upload(response, postData) {
     console.log("Request handler 'upload' was called.");
     response.writeHead(200, {"Content-Type": "text/plain"}); 
-    response.write("Hello Upload");
+    response.write("You've sent: "+ postData);
     response.end();
 }
 
